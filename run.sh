44b5d3faa695ed214c1685c98e9bc914d5072d55
Lord Vandal <27351045+lordvandal@users.noreply.github.com>
2025-01-24 13:54:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
+#/bin/bash
+
+if [[ ! -e /firehol/firehol.blocklist.cache ]]; then
+  cp /firehol-template/firehol.blocklist.cache /firehol
+fi
+
+if [[ ! -e /firehol/blocklist. ]]; then
+  cp /firehol-template/blocklist /firehol
+fi
+
+if [[ ! -e /firehol/whitelist ]]; then
+  cp /firehol-template/whitelist /firehol
+fi
+
 while :; do
     firehol-blocklists.sh
     sleep $(((((($RANDOM % 20)) + 50)) * 60)) # Sleeps for about 60 (+/- max 10) minutes (add random variable to avoid DDoS to iplist provider)
